4ecf3cbc79b40ac2aa2f008c21020cceee444d08
ClementTr <clement.tailleur@gmail.com>
2017-01-25 00:30:44 +0100

    Create root component for Android and IOS

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+class App extends  React.Component {
+    render() {
+        return (
+            <Text>This is a test</Text>
+        );
+    }
+}
+
+export default App;
