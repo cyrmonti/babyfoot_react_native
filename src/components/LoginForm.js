ced00e51e00fbdd8cdc06d7fef8337caa854ea18
ecaman <rlferreira94@gmail.com>
2017-01-25 23:02:52 +0100

    Set up router for app and add auth components in loginForm

---
+++ src/components/LoginForm.js
@@ -0,0 +1,92 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import { Card, CardSection, Input, Button } from './common';
+
+class LoginForm extends  React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: '',
+            password: '',
+            error: ''
+        };
+    }
+
+    updateEmailState(email) {
+        //remove extra start or end spaces
+        email = email.trim();
+        this.setState({ email });
+    }
+
+    onPressButton() {
+        const { email, password } = this.state;
+        this.setState({ error: '' });
+        console.log('Login press: ', email, password);
+        if (email !== '' && password !== '') {
+
+
+        }
+        else {
+            this.setState({ error: 'Empty field(s)' });
+        }
+    }
+
+    renderError() {
+        return (
+            <Text style={styles.errorTextStyle}>
+                {this.state.error}
+            </Text>
+        );
+    }
+
+    renderButton() {
+        return (
+            <Button onPress={this.onPressButton.bind(this)}>
+                Login
+            </Button>
+        );
+    }
+
+    render() {
+        return (
+            <Card>
+                <CardSection>
+                    <Input
+                        label={'Email'}
+                        value={this.state.email}
+                        onChangeText={this.updateEmailState.bind(this)}
+                        placeholder={'user@gmail.com'}
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input
+                        label={'Password'}
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                        placeholder={'enter password'}
+                        secureTextEntry
+                    />
+                </CardSection>
+
+                {this.renderError()}
+
+                <CardSection>
+                    {this.renderButton()}
+                </CardSection>
+            </Card>
+        );
+    }
+}
+
+
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+};
+
+export default LoginForm;
