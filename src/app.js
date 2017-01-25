c2dd4ae24b4476a4ca5970cc52be81ad9fbd0a8c
DarkMonti <monti.cyr@gmail.com>
2017-01-25 15:50:17 +0100

    Add Input email

--- src/app.js
+++ src/app.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import { Header, Card, CardSection } from './common';
+import { Header, Card, CardSection, Input } from './common';
 
 class App extends  React.Component {
+    constructor() {
+        super();
+        this.state = {
+            email: ''
+        };
+    }
+
+    updateEmailState(email) {
+        console.log(email);
+
+        //remove extra start or end spaces
+        email = email.trim();
+        this.setState({ email });
+    }
+
     render() {
         return (
             <View>
                 <Header headerText={'Authentification'} />
                 <Card>
                     <CardSection>
-
+                        <Input
+                            label={'Email'}
+                            value={this.state.email}
+                            onChangeText={this.updateEmailState.bind(this)}
+                            placeholder={'user@gmail.com'}
+                        />
                     </CardSection>
                 </Card>
             </View>
