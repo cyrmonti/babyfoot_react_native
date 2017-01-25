ca2615af8a72798028e1557db3ae9442ac4d6af0
DarkMonti <monti.cyr@gmail.com>
2017-01-25 15:10:15 +0100

    Add Card and CardSection

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-import { Header } from './common';
+import { Header, Card, CardSection } from './common';
 
 class App extends  React.Component {
     render() {
         return (
             <View>
                 <Header headerText={'Authentification'} />
+                <Card>
+                    <CardSection>
+
+                    </CardSection>
+                </Card>
             </View>
         );
     }
